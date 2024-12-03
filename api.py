c7da5293896b23d19b3e38ad950dd7fe47c2b072
Muhammad Umair <umairmateen55@gmail.com>
2024-12-03 15:27:19 +0500

    payload quotes

--- api.py
+++ api.py
@@ -17,9 +17,9 @@ class CheckInAPI:
         self.id = int()
     def login(self):
         headers = {
-            "sec-ch-ua-platform": "\"Linux\"",
+            "sec-ch-ua-platform": '"Linux"',
             "Referer": "https://linkedmatrix.resourceinn.com/",
-            "sec-ch-ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+            "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
             "sec-ch-ua-mobile": "?0",
             "Version-Code": "417",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
